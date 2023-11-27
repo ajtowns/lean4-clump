0520d3d5488186213623a64d4c50848fc1479df4
Anthony Towns <aj@erisian.com.au>
2023-11-28 02:26:56 +1000

    wip

--- Clump/Basic.lean
+++ Clump/Basic.lean
@@ -226,9 +226,68 @@ theorem Monotonic_app3: forall (r) (a b c : List α), (¬ b = []) → Monotonic 
       | cons hb tb => simp [Monotonic]; simp [Monotonic] at Hab; apply And.intro; apply Hab.left; rw [<-cons_append]; exact Hbc
     | cons ha2 ta2 => simp; rw [<-append_assoc]; simp [Monotonic]; simp [Monotonic] at Hab; apply And.intro; apply Hab.left; rw [<-append_assoc]; apply ta_ih; exact Hb; rw [cons_append]; apply Hab.right; apply Hbc
 
+/-- without loss of generality "tactic" so i don't repeat myself too much -/
+theorem wlog {α β : Type u} (P : α → α → Prop) [hP : @DecidableRel α P] (Q : β -> Prop) (X : α → α → β) :
+  (forall (a b), P a b -> Q (X a b)) -> (forall (a b), ¬ P a b -> P b a) -> forall (a b), Q (if (P a b) then X a b else X b a) := by
+  intro hPQ hnP a b; have hPab : Decidable (P a b); apply hP; cases hPab with
+  | isFalse hPab => simp [hPab]; apply hPQ; apply hnP; exact hPab
+  | isTrue hPab => simp [hPab]; apply hPQ; apply hPab
+
+/--
+ Layout is:
+    Cluster -- gives all the types and underlying representations
+    abbrev Chunking := List (List Tx)
+    abbrev Diagram := List Rat
+
+    PartialOrder (List Tx) -- compare by feerate
+
+    Raise : List Tx -> Chunking   -- raise each tx into [tx]
+    Diagram : Chunking -> Diagram
+    Optimise : Chunking -> Chunking
+    Drop : Chunking -> List Tx    -- concatenate the chunks
+
+    -- does Optimise [] return [] or [[]] ? does a Chunking ever have empty lists as elements?
+
+    Reorder (chunk other : List Tx) : List Tx -- reorder "chunk" to be in "other" order
+
+    PartialOrder Chunking  -- compare by diagram
+
+    theorems:
+      Perm (Reorder a o) a -- Reordering just reorders
+      Drop (Raise a) = a
+      Drop (Optimise a) = Drop a  -- order preserving
+      (Optimise a) >= a
+      (Optimise (Raise (Drop a))) >= a
+      Optimise (a ++ b) = Optimise ((Optimise a) ++ (Optimise b))
+      Optimise ((Optimise a) ++ b) = Optimise (a ++ b)
+      Optimise (a ++ (Optimise b)) = Optimise (a ++ b)
+
+      -- if a particular set of txs is the first chunk, optimising just those txs generates the same chunk
+      Optimise a = h::t -> Optimise [Raise h] = [h]
+
+      -- reordering a chunk only improves things
+      Optimise a = [[Drop a]] -> Perm (Drop a) (Drop b) -> Optimise b >= Optimise (Raise a)
+
+    pim-first base cmp:
+      Reorder (head (Optimise (Raise base))) cmp
 
 
 
+    prefix-intersection-merge:
+      L1, L2 : List Tx
+      HPerm : Perm L1 L2
+
+      c1 = pim-first L1 L2
+      c2 = pim-first L2 L1
+
+      if (c1 >= c2) then
+        (c1::prefix-intersection-merge (L1-c1) (L2-c1))
+      else
+        (c2::prefix-intersection-merge (L1-c2) (L2-c2))
+
+ oops, this is a docstring so better follow it up with something
+ -/
+theorem trivial: False -> True := by intro Hf; cases Hf
 
 -- theorem chunk_back_Monotonic: forall (lch) (ch : List Tx), Monotonic DiagramCmp.le lch -> Monotonic DiagramCmp.le (Chunk.chunk_back ch lch) := by
 --  sorry
